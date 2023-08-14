de91eb1a5aaff2c1ccae01f84ef4944a1ee1f465
zanuarprasetyo <122775305+zanuarprasetyo@users.noreply.github.com>
2023-08-15 05:27:50 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,20 @@
+  <?php
+  session_start();
+  if(!isset($_SESSION["username"])){
+    header("Location:index.php");
+  }
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php echo"<h1> Welcome".$_SESSION ["username"]. "</h1>";
+    ?>
+    <h1>Selamat Kamu Berhasil Login</h1>
+    <a href="logout.php">LOGOUT</a>
+</body>
+</html>
